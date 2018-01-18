23604869cd3f8da1d7d1eb0a753a5c8e315805b1
adamredwoods <adamredwoods@users.noreply.github.com>
2018-01-18 09:33:53 -0800

    checks for non-ints

--- src/calculator.js
+++ src/calculator.js
@@ -9,25 +9,44 @@ class Calculator extends Component {
          val2: 0,
          val3: 0,
          val4: 0,
+         val5: 0,
+         val6: 0,
          calcTotalAdd: 0,
-         calcTotalSub: 0
+         calcTotalSub: 0,
+         calcTotalMult: 0
       }
 
       this.onChangeAdd = this.onChangeAdd.bind(this);
+      //this.onChangeAdd2 = this.onChangeAdd2.bind(this);
       this.onChangeSub = this.onChangeSub.bind(this);
+      this.onChangeMult = this.onChangeMult.bind(this);
    };
 
    onChangeAdd(e) {
-      if(!e.target.value) return;
+      if(!e.target.value || !parseInt(e.target.value)) return;
       this.setState({[e.target.name]: e.target.value});
       this.setState((prev)=>({calcTotalAdd: parseInt(prev.val1)+parseInt(prev.val2)}));
+      //this.setState({calcTotalAdd: parseInt(this.state.val1)+parseInt(this.state.val2)})
+   }
+
+   // onChangeAdd2(e) {
+   //    if(!e.target.value) return;
+   //    this.setState({val2: e.target.value});
+   //    //this.setState((prev)=>({calcTotalAdd: parseInt(prev.val1)+parseInt(prev.val2)}));
+   //    this.setState({calcTotalAdd: parseInt(this.state.val1)+parseInt(e.target.value)})
+   // }
+
+   onChangeSub(e) {
+      if(!e.target.value || !parseInt(e.target.value)) return;
+      this.setState({[e.target.name]: e.target.value});
+      this.setState((prev)=>({calcTotalSub: parseInt(prev.val3)-parseInt(prev.val4)}));
 
    }
 
-   onChangeSub(e) {
-      if(!e.target.value) return;
+   onChangeMult(e) {
+      if(!e.target.value || !parseInt(e.target.value)) return;
       this.setState({[e.target.name]: e.target.value});
-      this.setState((prev)=>({calcTotalSub: parseInt(prev.val3)-parseInt(prev.val4)}));
+      this.setState((prev)=>({calcTotalMult: parseInt(prev.val5)*parseInt(prev.val6)}));
 
    }
 
@@ -52,6 +71,15 @@ class Calculator extends Component {
                <span>=</span>
                <div className="">
                   <h3>{this.state.calcTotalSub}</h3>
+               </div>
+            </div>
+            <div className="mult">
+               <input type="text" className="text-input" name="val5" onChange={this.onChangeMult}/>
+               <span>*</span>
+               <input type="text" className="text-input" name="val6" onChange={this.onChangeMult}/>
+               <span>=</span>
+               <div className="">
+                  <h3>{this.state.calcTotalMult}</h3>
                </div>
             </div>
          </div>
