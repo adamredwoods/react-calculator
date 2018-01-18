bd9c5c43ef335d1b35dfdad84845eb37b850d736
adamredwoods <adamredwoods@users.noreply.github.com>
2018-01-18 01:27:34 -0800

    implemented subtract

--- src/calculator.js
+++ src/calculator.js
@@ -7,16 +7,27 @@ class Calculator extends Component {
       this.state = {
          val1: 0,
          val2: 0,
-         calcTotal: 0
+         val3: 0,
+         val4: 0,
+         calcTotalAdd: 0,
+         calcTotalSub: 0
       }
 
-      this.onChange = this.onChange.bind(this);
+      this.onChangeAdd = this.onChangeAdd.bind(this);
+      this.onChangeSub = this.onChangeSub.bind(this);
    };
 
-   onChange(e) {
+   onChangeAdd(e) {
       if(!e.target.value) return;
       this.setState({[e.target.name]: e.target.value});
-      this.setState((prev)=>({calcTotal: parseInt(prev.val1)+parseInt(prev.val2)}));
+      this.setState((prev)=>({calcTotalAdd: parseInt(prev.val1)+parseInt(prev.val2)}));
+
+   }
+
+   onChangeSub(e) {
+      if(!e.target.value) return;
+      this.setState({[e.target.name]: e.target.value});
+      this.setState((prev)=>({calcTotalSub: parseInt(prev.val3)-parseInt(prev.val4)}));
 
    }
 
@@ -26,12 +37,21 @@ class Calculator extends Component {
             <h1>Add with React!</h1>
 
             <div className="add">
-               <input type="text" className="text-input" name="val1" onChange={this.onChange}/>
+               <input type="text" className="text-input" name="val1" onChange={this.onChangeAdd}/>
                <span>+</span>
-               <input type="text" className="text-input" name="val2" onChange={this.onChange}/>
+               <input type="text" className="text-input" name="val2" onChange={this.onChangeAdd}/>
                <span>=</span>
                <div className="">
-                  <h3>{this.state.calcTotal}</h3>
+                  <h3>{this.state.calcTotalAdd}</h3>
+               </div>
+            </div>
+            <div className="minus">
+               <input type="text" className="text-input" name="val3" onChange={this.onChangeSub}/>
+               <span>-</span>
+               <input type="text" className="text-input" name="val4" onChange={this.onChangeSub}/>
+               <span>=</span>
+               <div className="">
+                  <h3>{this.state.calcTotalSub}</h3>
                </div>
             </div>
          </div>
